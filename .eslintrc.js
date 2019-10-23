5053c7693690949367e6f55fd593fd41ed4831c7
tsuyoshiwada <wadackel@gmail.com>
2019-10-24 01:52:41 +0900

    chore: Tweak build config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: 'tsconfig.json',
     ecmaFeatures: {
       jsx: true,
     },
